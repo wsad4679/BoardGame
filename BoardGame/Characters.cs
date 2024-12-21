75a31a4d92588b596bb3b8e089b4ddb8d362de40
SzymonH <szymonh4679@gmail.com>
2024-12-21 23:38:58 +0100

    Create part of Characters functions

--- BoardGame/Characters.cs
+++ BoardGame/Characters.cs
@@ -1,40 +1,64 @@
 namespace Characters;
-
-interface CharacterType
+using Gameplay;
+interface ICharacterType
 {
     public void SpecialPower();
 
 }
 
-class Mage : CharacterType
+class Mage : ICharacterType
 {
-    public void SpecialPower()
+    public int HP = 300;
+    public int ATK = 100;
+    public void SpecialPower()// throw spell
     {
-        throw new NotImplementedException();
+        void MoveAgain()
+        {
+            //playerTurn()
+        }
     }
 
 
 }
 
-class Warrior : CharacterType
+class Warrior : ICharacterType
 {
-    public void SpecialPower()
+    public int HP = 500;
+    public int ATK = 200;
+    public void SpecialPower()// stronger attack
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Czy chcesz użyć: heroiczny atak (rzucasz kostką od 0 do 4, tyle ile wypadnie taki mnożnik twojego ataku) tak/nie");
+        string choice = Console.ReadLine();
+
+        if (choice == "tak")
+        {
+            int attackMultiply = Dice.RollAttackMultiply();
+            //attack()
+        }
+        else
+        {
+            //attack()
+        }
+        
+        
     }
 
 
 }
 
-class Healer : CharacterType
+class Healer : ICharacterType
 {
+    public int HP = 400;
+    public int ATK = 50;
     public void SpecialPower()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Użyto specjalnej umiejętności: łaska boga, przywrócono HP do 400");
+        HP = 400;
     }
 
     public void PassiveSkill()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Passywna umiejętność: zyskano 50 HP.");
+        HP += 50;
     }
 }
