8511f225c28a0fb67e0d1a84d64c48c0a6f8cca7
SzymonH <szymonh4679@gmail.com>
2024-12-22 11:20:21 +0100

    Finished SpecialPower methods in characters classes

--- BoardGame/Characters.cs
+++ BoardGame/Characters.cs
@@ -2,7 +2,8 @@ namespace Characters;
 using Gameplay;
 interface ICharacterType
 {
-    public void SpecialPower();
+    public int SpecialPower();
+    public void PassiveSkill();
 
 }
 
@@ -10,22 +11,42 @@ class Mage : ICharacterType
 {
     public int HP = 300;
     public int ATK = 100;
-    public void SpecialPower()// throw spell
+    private int SPUse = 2;
+    public int  SpecialPower()// throw spell
     {
-        void MoveAgain()
+        if (SPUse > 0)
         {
-            //playerTurn()
+            Console.WriteLine(
+                $"Czy chcesz użyć swojej umięjętności ponowny ruch (tak/nie). Zosatły ci {SPUse} użycia.");
+            var isUsed = Console.ReadLine();
+            if (isUsed == "tak")
+            {
+                SPUse--;
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
+        else
+        {
+            return 0;
+        }
+
     }
 
-
+    public void PassiveSkill()
+    {
+        //ja zakładam brak, chyba że znajdziesz jakiś pomysł
+    }
 }
 
 class Warrior : ICharacterType
 {
     public int HP = 500;
     public int ATK = 200;
-    public void SpecialPower()// stronger attack
+    public int SpecialPower()// stronger attack
     {
         Console.WriteLine("Czy chcesz użyć: heroiczny atak (rzucasz kostką od 0 do 4, tyle ile wypadnie taki mnożnik twojego ataku) tak/nie");
         string choice = Console.ReadLine();
@@ -33,23 +54,33 @@ class Warrior : ICharacterType
         if (choice == "tak")
         {
             int attackMultiply = Dice.RollAttackMultiply();
-            //attack()
+            return attackMultiply;
+            
         }
-       
+        else
+        {
+            return 1;
+        }
         
     }
 
-
+    public void PassiveSkill()
+    {
+        //ja zakładam brak, chyba że znajdziesz jakiś pomysł
+    }
 }
 
 class Healer : ICharacterType
 {
     public int HP = 400;
     public int ATK = 50;
-    public void SpecialPower()
+    public int SPUse = 1;
+    public int SpecialPower()
     {
         Console.WriteLine("Użyto specjalnej umiejętności: łaska boga, przywrócono HP do 400");
         HP = 400;
+        SPUse--;
+        return 0;
     }
 
     public void PassiveSkill()
