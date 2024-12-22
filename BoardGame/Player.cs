e7a12ecb584842643a3e2511da3bf7b9c68db203
SzymonH <szymonh4679@gmail.com>
2024-12-22 01:05:31 +0100

    Finished Player class, possibly it will be modified

--- BoardGame/Player.cs
+++ BoardGame/Player.cs
@@ -1,11 +1,17 @@
 namespace Characters;
-
 class Player
 {
     private string Name { get; set; }
     private int Position = 0;
     private int Score = 0;
-    public int Character { get; set; }
+    public ICharacterType Character { get; set; }
+
+    public Player(string name)
+    {
+        Name = name;
+        Position = new Random().Next(0, 25);
+        Character = ChooseCharacter();
+    }
 
     private void UpdateScore(int addScore)
     {
@@ -15,9 +21,31 @@ class Player
     private int characterMove(int diceOutcome)
     {
         Position += diceOutcome;
-        //if position > rozmiar planszy
-        //position = position - rozmiar planszy
+        if (Position > 25)
+        {
+            Position -= 25;
+        }
+
         return Position;
     }
+
+    private ICharacterType ChooseCharacter()
+    {
+        Console.WriteLine("Wybierz klasę swojej postaci: Mage, Warrior, Healer");
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case "Mage":
+                return new Mage();
+            case "Warrior":
+                return new Warrior();
+            case "Healer":
+                return new Healer();
+            default:
+                Console.WriteLine("Nie ma takiej postaci. Wybrano domyślnie Warrior.");
+                return new Warrior();
+        }
+    }
+
 
 }
